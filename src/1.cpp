6363042415486bc12ed6b795ff997021d8b320b0
chlouatoqm <chlouatoqm@gmx.com>
2025-03-05 04:00:10 +0000

    Add 1.cpp

---
+++ src/1.cpp
@@ -0,0 +1,19 @@
+  int main() {
+    // Declare variables
+    int x = 0;
+    float y = 0.0f;
+  
+    // Ask user to input values for x and y
+    cout << "Enter value of x: ";
+    cin >> x;
+    cout << "Enter value of y: ";
+    cin >> y;
+  
+    // Calculate the result
+    float result = x + y;
+  
+    // Print the result
+    cout << "Result is: " << result << endl;
+  
+    return 0;
+  }
